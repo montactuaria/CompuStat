1ebdaa276f10f2d76056046753901b97b8589f6f
montactuaria <montactuaria@gmail.com>
2015-09-07 19:46:49 -0500

    modificaciones

--- FuncionInversa/server.R
+++ FuncionInversa/server.R
@@ -1,20 +1,18 @@
 library(shiny)
 library(ggplot2)
 
-rExp <- function(nsim, lambda = 1){
+rExp <- function(nsim, lambda){
   return((-1/lambda)*log(1-runif(nsim)))
 }
 
 shinyServer(function(input, output){
   
-  dat <- data.frame(Value=rExp(nsim, lambda))
+  dat <- reactive(rExp(input$nsim,input$lambda))
   
-  output$table <- renderDataTable(dat)
+  output$table <- renderDataTable(data.frame(value = dat()))
   
   output$plot <- renderPlot(
-            ggplot(dat, aes(x=Value)) +
-            geom_histogram(aes(y=..density..), binwidth= .2, colour="black", fill="white")+
-            stat_function(fun = function(x) lambda*exp(-lambda*x),colour = "blue")
-            )
-  
+    qplot(dat(), fill="blue")
+          )
+    
 })
